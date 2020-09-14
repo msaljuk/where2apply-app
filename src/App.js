8a29a1bb3476b1d289f2326f184656fcaf6516a0
msaljuk <msaljuk@stanford.edu>
2020-09-14 07:50:43 +0500

    Add Analytics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
+import ReactGA from "react-ga";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Match from "./pages/Match/Match";
 
+const trackingId = "UA-144735539-2";
+ReactGA.initialize(trackingId);
+
+const history = createBrowserHistory();
+history.listen((location) => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <Header />
       <Switch>
         <Route exact path="/">
